c52c1ba3ea1a5109ea7f78b23ab271032567e23a
cwl <531043521@qq.com>
2022-02-13 12:29:24 +0800

    first commitidea

---
+++ src/main/java/com/cwl/pushgit/PushgitApplication.java
@@ -0,0 +1,13 @@
+package com.cwl.pushgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushgitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cwl/pushgit/xx.java
@@ -0,0 +1,13 @@
+package com.cwl.pushgit;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: cwl
+ * @date: 2022/02/13/12:27
+ */
+public class xx {
+    public static void main(String[] args) {
+        System.out.println("helli");
+    }
+}
